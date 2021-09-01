d9488dc385214bb7f01e35c81512aa0fbc4fca64
Lukasz <lukjut@gmail.com>
2021-09-01 18:30:49 +0300

    Add project files.

---
+++ Utils/Globals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Ikrito_Fulfillment_Platform.Utils {
+    static class Globals {
+        // Shopify app creds
+        private static string shopifyAPIKey = "40a65778e811be7da9d06d9dcc6d7e8e";
+        private static string shopifyAPIPass = "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";
+
+        public static string getBase64ShopifyCreds() {
+            string preEncodeCreds = shopifyAPIKey + ":" + shopifyAPIPass;
+            byte[] textBytes = Encoding.UTF8.GetBytes(preEncodeCreds);
+            string encodedCreds = Convert.ToBase64String(textBytes);
+
+            return "Basic " + encodedCreds;
+        }
+
+    }
+}
