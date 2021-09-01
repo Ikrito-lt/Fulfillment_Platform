11a398451ea6155b07f7627e2799b207e85815cc
Lukasz <lukjut@gmail.com>
2021-09-01 18:30:49 +0300

    Add project files.

---
+++ Modules/OrderGetter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Ikrito_Fulfillment_Platform.Utils;
+using Newtonsoft.Json;
+
+namespace Ikrito_Fulfillment_Platform.Modules {
+    class OrderGetter {
+        private readonly string getOrdersEndPoint = "https://real-europe-corp.myshopify.com/admin/api/2021-07/orders.json";
+        
+        public string getOrders() {
+
+            var client = new RestClient(getOrdersEndPoint);
+            var request = new RestRequest();
+            request.AddHeader("Authorization", Globals.getBase64ShopifyCreds());
+
+            var response = client.Get(request);
+
+            if (response.IsSuccessful) {
+                string content = response.Content; // Raw content as string
+                return content;
+            } else {
+                return null;
+            }
+        }
+
+
+    }
+}
