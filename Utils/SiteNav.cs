87c063a3c35dd7a6174069a0dc5df2c2d99a893e
Lukasz <lukjut@gmail.com>
2021-10-12 22:42:40 +0300

    fixed adding imagfes and tags to product

---
+++ Utils/SiteNav.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ikrito_Fulfillment_Platform.Utils {
+    static class SiteNav {
+
+        public static void GoToSite(string url) {
+            System.Diagnostics.Process.Start("explorer.exe", url);
+        }
+    }
+}
