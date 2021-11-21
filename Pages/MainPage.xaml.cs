8206059a3962078b5065b1cfebef835fb1b3a865
Lukasz <lukjut@gmail.com>
2021-11-21 23:58:28 +0200

    remade product edit page.

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using Ikrito_Fulfillment_Platform.Modules;
 using Ikrito_Fulfillment_Platform.Pages;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -21,24 +22,36 @@ namespace Ikrito_Fulfillment_Platform {
             RefreshNewOrderDG();
         }
 
+        //
+        // Data grid manipulation section
+        //
+
+        //changes text to say howm much orders there is
         private void UpdateNewOrderLabel(int count) { 
             newOrderCountL.Content = "Current New Orders: " + count.ToString();
         } 
 
+        //loads orders to order grid
         private void RefreshNewOrderDG() {
             newOrderDG.ItemsSource = null;
             newOrders.Clear();
             newOrders.AddRange(newOrderGetter.getOrders());
 
             UpdateNewOrderLabel(newOrders.Count);
-
-            newOrderDG.ItemsSource = newOrders;
+            newOrderDG.ItemsSource = newOrders.ToList();
         }
 
+        //Todo: redo button to make it double click
         private void ShowOrderInfo(object sender, RoutedEventArgs e) {
-            //Todo: redo button
+            
         }
 
+
+        //
+        // Page change section
+        // 
+
+        //opens page with all products
         private void openBrowserPage(object sender, RoutedEventArgs e) {
             MainWindow.Instance.mainFrame.Content = ProductBrowsePage.Instance;
         }
--- UI/CheckBoxListItem.cs
+++ UI/CheckBoxListItem.cs
@@ -1,13 +1,4 @@
-﻿using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ikrito_Fulfillment_Platform.UI {
+﻿namespace Ikrito_Fulfillment_Platform.UI {
     public class CheckBoxListItem{
 
         public string Name {
