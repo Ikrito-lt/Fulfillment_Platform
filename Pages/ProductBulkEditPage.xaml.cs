78cb0cd2a7007b0a64ed4e4d63d880572d548206
Lukasz <lukjut@gmail.com>
2022-01-10 18:30:45 +0200

    making bulk edit p1

---
+++ Pages/ProductBulkEditPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Ikrito_Fulfillment_Platform.Models;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ikrito_Fulfillment_Platform.Pages
+{
+    public partial class ProductBulkEditPage : Page
+    {
+        private readonly List<Product> Products;
+        private readonly Page PreviousPage;
+        private readonly Dictionary<string, string> CategoryKVP;
+
+        public ProductBulkEditPage(List<Product> products, Dictionary<string, string> categoryKVP, Page prevPage)
+        {
+            InitializeComponent();
+            PreviousPage = prevPage;
+            Products = products;
+            CategoryKVP = categoryKVP;
+        }
+
+        //
+        // Buttons section
+        //
+
+        /// <summary>
+        /// Button to go back
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.mainFrame.Content = PreviousPage;
+        }
+
+        private void ChangeTypesButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
